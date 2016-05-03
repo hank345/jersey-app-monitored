174591fca1469a69252b464bff65073fa647afbc
henrik <geijstedt@telia.com>
2016-05-03 11:43:30 +0200

    Trying out adding a addLifeCycleListener to context handler in order to get stage-monitor to work.

--- src/main/java/se/henriks/webapp/Main.java
+++ src/main/java/se/henriks/webapp/Main.java
@@ -3,9 +3,13 @@ package se.henriks.webapp;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Slf4jLog;
-import org.stagemonitor.core.Stagemonitor;
+import org.stagemonitor.web.WebPlugin;
+
+import javax.servlet.ServletException;
 
 /**
  * Entry point for testing Jersey REST service powered by Jetty, monitored with stagemonitor
@@ -15,7 +19,7 @@ public class Main {
     private static final String REST_RESOURCE_PACKAGE = "se.henriks.webapp.resource";
 
     public static void main(String[] args) throws Exception {
-        Stagemonitor.init(); // Start monitoring this app
+
         startServer();
     }
 
@@ -23,16 +27,27 @@ public class Main {
 
         // Jetty Server
         Server server = new Server(8080);
-        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        final ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         contextHandler.setContextPath("/");
         server.setHandler(contextHandler);
+
+        contextHandler.addLifeCycleListener(new AbstractLifeCycle.AbstractLifeCycleListener() {
+            @Override
+            public void lifeCycleStarting(LifeCycle event) {
+                try {
+                    new WebPlugin().onStartup(null, contextHandler.getServletContext());
+                } catch (ServletException e) {
+                    e.printStackTrace();
+                }
+            }Tr
+        });
 
         // Set up Jersey
         ServletHolder jerseyServlet = contextHandler.addServlet(
                 org.glassfish.jersey.servlet.ServletContainer.class, "/*");
 
         // Tells the Jersey Servlet which REST service/class(es) to load.
-        jerseyServlet.setInitParameter("jersey.config.server.provider.packages",  REST_RESOURCE_PACKAGE);
+        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", REST_RESOURCE_PACKAGE);
         jerseyServlet.setInitOrder(0);
 
         // Use SLF4J as logging API.
--- src/main/java/se/henriks/webapp/resource/HelloWorldResource.java
+++ src/main/java/se/henriks/webapp/resource/HelloWorldResource.java
@@ -3,7 +3,7 @@ package se.henriks.webapp.resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.stagemonitor.requestmonitor.MonitorRequests;
+
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,7 +12,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
-@MonitorRequests
 @Path("/")
 public class HelloWorldResource {
 
